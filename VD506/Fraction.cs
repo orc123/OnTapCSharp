0a66a1d821f3537df4474588c0e4418c2c0b4ce0
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-02-07 07:55:26 +0700

    thêm VD 5.6

---
+++ VD506/Fraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace VD506;
+
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder s = new StringBuilder();
+        s.AppendFormat($"{numerator}/{denominator}");
+        return s.ToString();
+    }
+
+    internal class FractionArtist
+    {
+        public void Draw(Fraction f)
+        {
+            Console.WriteLine("Drawing the numerator {0}", f.numerator);
+            Console.WriteLine("Drawing the denominator {0}", f.denominator);
+        }
+    }
+}
---
+++ VD506/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace VD506
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(3, 4);
+            Console.WriteLine("f1: " + f1.ToString());
+
+            Fraction.FractionArtist fa = new Fraction.FractionArtist();
+            fa.Draw(f1);
+        }
+    }
+}
