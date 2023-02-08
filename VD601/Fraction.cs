06c692b603f454d1ffd99fb0385840cd2a58e1cc
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-02-08 23:25:22 +0700

    Chương 6

---
+++ VD601/Fraction.cs
@@ -0,0 +1,74 @@
+﻿namespace VD601;
+
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+    public Fraction(int wholeNumber)
+    {
+        Console.WriteLine($"In Fraction Constructor(int)");
+        this.numerator = wholeNumber;
+        this.denominator = 1;
+    }
+    public Fraction(int numerator, int denominator)
+    {
+        Console.WriteLine($"In Fraction Constructor(int, int)");
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public static implicit operator Fraction(int theInt)
+    {
+        Console.WriteLine("In implicit conversion to Fraction");
+        return new Fraction(theInt);
+    }
+    public static explicit operator int(Fraction theFraction)
+    {
+        Console.WriteLine("In explicit conversion to int");
+        return theFraction.numerator / theFraction.denominator;
+    }
+    public static bool operator == (Fraction lhs, Fraction rhs)
+    {
+        Console.WriteLine("In operator ==");
+        if (lhs.numerator == rhs.numerator && lhs.denominator == rhs.numerator)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool operator !=(Fraction lhs, Fraction rhs)
+    {
+        Console.WriteLine("In operator !=");
+        return !(lhs == rhs);
+    }
+
+    public override bool Equals(object o)
+    {
+        Console.WriteLine("In method Equals");
+        if (!(o is Fraction))
+        {
+            return false;
+        }
+        return this == (Fraction)o;
+    }
+    public static Fraction operator +(Fraction lhs, Fraction rhs)
+    {
+        Console.WriteLine("In operator +");
+        if (lhs.denominator == rhs.denominator)
+        {
+            return new Fraction(lhs.numerator + rhs.numerator, lhs.denominator);
+        }
+        //thực hiện khi hai mẫu số khộng bằng nhau
+        int firstProduct = lhs.numerator * rhs.denominator;
+        int secondProduct = rhs.numerator * lhs.denominator;
+        return new Fraction(firstProduct + secondProduct,
+        lhs.denominator * rhs.denominator);
+    }
+
+    public override string ToString()
+    {
+        return numerator.ToString() + "/" + denominator.ToString();
+    }
+
+}
---
+++ VD601/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace VD601
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f1 = new Fraction(3, 4);
+            Console.WriteLine("f1:{0}", f1.ToString());
+
+            Fraction f2 = new Fraction(2, 4);
+            Console.WriteLine("f2:{0}", f2.ToString());
+
+            Fraction f3 = f1 + f2;
+            Console.WriteLine("f1 + f2 = f3:{0}", f3.ToString());
+
+            Fraction f4 = f3 + 5;
+            Console.WriteLine("f4 = f3 + 5:{0}", f4.ToString());
+
+            Fraction f5 = new Fraction(2, 4);
+            if (f5 == f2)
+            {
+                Console.WriteLine("f5:{0}==f2:{1}",
+                f5.ToString(), f2.ToString());
+            }
+
+        }
+    }
+}
