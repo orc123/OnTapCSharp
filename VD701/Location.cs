af4ab94e5719b4a00a5791628bb962bd7103ffc1
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-02-09 20:50:19 +0700

    chương 7 struct

---
+++ VD701/Location.cs
@@ -0,0 +1,42 @@
+﻿namespace VD701;
+
+public struct Location
+{
+    private int xVal;
+    private int yVal;
+    public Location(int xCoordinate, int yCoordinate)
+	{
+        xVal = xCoordinate;
+        yVal = yCoordinate;
+	}
+
+    public int X
+    {
+        get
+        {
+            return xVal;
+        }
+        set
+        {
+            xVal = value;
+        }
+    }
+
+    public int Y
+    {
+        get
+        {
+            return yVal;
+        }
+        set
+        {
+            yVal = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return (String.Format("{0}, {1}", xVal, yVal));
+    }
+}
+
---
+++ VD701/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace VD701;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Location loc1 = new Location(200, 300);
+        Console.WriteLine("Loc1 location: {0}", loc1);
+
+        Program program = new Program();
+        program.myFunc(loc1);
+        Console.WriteLine("Loc1 location: {0}", loc1);
+    }
+
+    public void myFunc(Location location)
+    {
+        location.X = 50;
+        location.Y = 100;
+        Console.WriteLine("Loc1 location: {0}", location);
+    }
+}
