a80bfca2e43595d0345d76fc2f6c1195cc7ce968
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-02-18 23:05:53 +0700

    mảng đa chiều

---
+++ VD904/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace VD904;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        const int rows = 4;
+        const int columns = 3;
+
+        int[,] rectangularArray = new int[rows, columns];
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                rectangularArray[i, j] = i + j;
+            }
+        }
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"rectangularArray[{i}, {j}] = {rectangularArray[i, j]}");
+            }
+        }
+    }
+}
---
+++ VD905/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace VD905;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        const int rows = 4;
+        const int columns = 3;
+
+        int[,] rectangularArray =
+        {
+            {0, 1, 2 },
+            {3, 4, 5 },
+            {6, 7, 8},
+            {9, 10, 11 }
+        };
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"rectangularArray[{i}, {j}] = {rectangularArray[i, j]}");
+            }
+        }
+    }
+}
