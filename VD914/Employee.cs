ff19c8f6b070f6516c02f4a62d6d3c069ba939a5
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-02-23 22:00:08 +0700

    marng cuooi

---
+++ VD914/Employee.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+
+namespace VD914;
+
+public class Employee : IComparable
+{
+    private int empId;
+    private int yearsOfSrc = 1;
+    public Employee(int empId)
+    {
+        this.empId = empId;
+    }
+    public Employee(int empId, int yearsOfSrc)
+    {
+        this.empId = empId;
+        this.yearsOfSrc = yearsOfSrc;
+    }
+
+    public override string ToString()
+    {
+        return $"Id: {empId.ToString()}. Years of Svc: {yearsOfSrc.ToString()}";
+    }
+
+    public static EmployeeComparer GetComparer()
+    {
+        return new Employee.EmployeeComparer();
+    }
+
+    public int CompareTo(object obj)
+    {
+        Employee r = (Employee)obj;
+        return this.empId.CompareTo(r.empId);
+    }
+
+    public int CompareTo(Employee rhs, EmployeeComparer.ComparisionType which)
+    {
+        switch (which)
+        {
+            case Employee.EmployeeComparer.ComparisionType.EmpId:
+                return this.empId.CompareTo(rhs.empId);
+            case Employee.EmployeeComparer.ComparisionType.Years:
+                return this.empId.CompareTo(rhs.yearsOfSrc);
+        }
+        return 0;
+    }
+    public class EmployeeComparer : IComparer
+    {
+        private EmployeeComparer.ComparisionType whichComparision;
+        public EmployeeComparer.ComparisionType WhichComparision
+        {
+            get { return whichComparision; }
+            set { whichComparision = value; }
+        }
+        public enum ComparisionType
+        {
+            EmpId,
+            Years
+        }
+        public int Compare(object lhs, object rhs)
+        {
+            Employee l = (Employee)lhs;
+            Employee r = (Employee)rhs;
+            return l.CompareTo(r, WhichComparision);
+        }
+    }
+
+
+}
---
+++ VD914/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace VD914;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        ArrayList empArray = new ArrayList();
+        Random random= new Random();
+        for (int i = 0; i < 5; i++)
+        {
+            empArray.Add(new Employee(random.Next(10) + 100, random.Next(20)));
+        }
+        for (int i = 0; i < empArray.Count; i++)
+        {
+            Console.Write($"\n{empArray[i].ToString()}");
+        }
+        Console.WriteLine("\n");
+        Employee.EmployeeComparer c = new Employee.EmployeeComparer();
+        c.WhichComparision = Employee.EmployeeComparer.ComparisionType.EmpId;
+        empArray.Sort(c);
+        for (int i = 0; i < empArray.Count; i++)
+        {
+            Console.Write($"\n{empArray[i].ToString()}");
+        }
+        Console.WriteLine("\n");
+        c.WhichComparision = Employee.EmployeeComparer.ComparisionType.Years;
+        empArray.Sort(c);
+        for (int i = 0; i < empArray.Count; i++)
+        {
+            Console.Write($"\n{empArray[i].ToString()}");
+        }
+        Console.WriteLine("\n");
+
+    }
+}
