9e8a6d2a89ed90f6568909cd4631c8d55d3c11c5
Dong Pham <dong.orc12@gmail.com>
2023-02-26 21:36:07 +0700

    string tiếp theo

---
+++ VD1005/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace VD1005;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string s1 = "Mot, hai, ba, Trung tam dao tao CNTT";
+
+        Regex theRegex = new Regex(" |, ");
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int id = 1;
+
+        //foreach (string subString in theRegex.Split(s1)) VD10.5
+        foreach (string subString in Regex.Split(s1, " |, "))
+        {
+            stringBuilder.Append($"{id++}: {subString} \n");
+        }
+
+        Console.WriteLine(stringBuilder);
+    }
+}
